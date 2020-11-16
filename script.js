ace79752071d581e10bf56c78977e12797e66732
Joel Julius <okmarq@gmail.com>
2020-11-17 00:43:58 +0100

    second wiki commit

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-{}
+$(document).ready(function() {
+  $('#search').click(function() {
+    var searchTerm = $("#searchTerm").val();
+    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
+    $.ajax({
+      type: "GET",
+      url: url,
+      async: false,
+      dataType: "json",
+      success: function(data) {
+        $('#output').html('')
+        for (var i = 0; i < data[1].length; i++) {
+          $("#output").prepend("<li><a href=" + data[3][i] + ">" + data[1][i] + "</a><p>" + data[2][i] + "</p></li>");
+        }
+        $("#searchTerm").val('');
+      },
+      error: function(errorMessage) {
+        alert("Error");
+      }
+    });
+
+  });
+});
+
+$("#searchTerm").keypress(function(e) {
+  if (e.which == 13) {
+    $('#search').click();
+  }
+});
